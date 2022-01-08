f720ef2e4f15ddd43b09da0c00629fe535665714
Alex-Lar <doremilarin@gmail.com>
2022-01-08 06:33:36 +0300

    notes & deletes

--- app.js
+++ app.js
@@ -13,6 +13,28 @@ startBtn.addEventListener('click', function() {
             notesTitle.unshift(userQuery);
             userQuery = prompt('Теперь вы можете писать!', '');
             notesContent.unshift(userQuery);
+        } 
+        else if (userQuery === 'titles') {
+            console.log('*******************');
+            console.log('Мои записи:');
+            for (let i = 0; i <= notesTitle.length - 1; i++) {
+                console.log(`${i}. ${notesTitle[i]}`);
+            }
+            console.log('*******************');
+        }
+        else if (userQuery === 'note') {
+            userQuery = prompt('Введите номер записи', '');
+            console.log('*******************');
+            console.log('Ваша записка:');
+            console.log(notesTitle[userQuery]);
+            console.log(notesContent[userQuery]);
+            console.log('*******************');
+        }
+        else if (userQuery === 'delete') {
+            userQuery = prompt('Введите номер записи, которою хотите удалить', '');
+            console.log(`Запись "${notesTitle[userQuery]}" удалена.`);
+            notesTitle.splice(userQuery, 1);
+            notesContent.splice(userQuery, 1);
         }
     }
 });
