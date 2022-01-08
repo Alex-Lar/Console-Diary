07b617ed85688283fcb6153660744d06aca2428b
Alex-Lar <doremilarin@gmail.com>
2022-01-08 06:46:31 +0300

    en lang

--- app.js
+++ app.js
@@ -5,34 +5,35 @@ let startBtn = document.querySelector('#btn'),
 
 
 startBtn.addEventListener('click', function() {
+    userQuery = 1;
     while(userQuery !== 'quit') {
-        userQuery = prompt('Введите запрос', '').toLowerCase();
+        userQuery = prompt('Enter your request', '').toLowerCase();
 
         if (userQuery === 'new') {
-            userQuery = prompt('Введите заголовок записи', '');
+            userQuery = prompt('Enter a title for your note', '');
             notesTitle.unshift(userQuery);
-            userQuery = prompt('Теперь вы можете писать!', '');
+            userQuery = prompt('You can write a note', '');
             notesContent.unshift(userQuery);
         } 
         else if (userQuery === 'titles') {
             console.log('*******************');
-            console.log('Мои записи:');
+            console.log('My notes:');
             for (let i = 0; i <= notesTitle.length - 1; i++) {
                 console.log(`${i}. ${notesTitle[i]}`);
             }
             console.log('*******************');
         }
         else if (userQuery === 'note') {
-            userQuery = prompt('Введите номер записи', '');
+            userQuery = prompt('Enter the note number', '');
             console.log('*******************');
-            console.log('Ваша записка:');
+            console.log('Note:');
             console.log(notesTitle[userQuery]);
             console.log(notesContent[userQuery]);
             console.log('*******************');
         }
         else if (userQuery === 'delete') {
-            userQuery = prompt('Введите номер записи, которою хотите удалить', '');
-            console.log(`Запись "${notesTitle[userQuery]}" удалена.`);
+            userQuery = prompt('Enter the number of the note you want to delete', '');
+            console.log(`Note "${notesTitle[userQuery]}" deleted.`);
             notesTitle.splice(userQuery, 1);
             notesContent.splice(userQuery, 1);
         }
